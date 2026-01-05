ff75c3ebee244d3c7d0989fb6df82ae96055abc7
Elimelech Ehrlich <ee525566@gmail.com>
2026-01-05 18:02:13 +0200

    create all endpoints

--- midlewares/usersM.js
+++ midlewares/usersM.js
@@ -71,23 +71,6 @@ async function checkTrap(req, res, next) {
     }
 }
 
-
-
-// function isArrayInAscendingOrder(arr) {
-//   if (arr.length <= 1) {
-//     return true;
-//   }
-
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     if (arr[i] > arr[i + 1]) {
-//       return false;
-//     }
-//   }
-
-//   return true; 
-// }
-
-
 export {
     validateFieldsInbody,
     checkIfUserNotExsist,
